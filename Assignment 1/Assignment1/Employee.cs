f3c9df614248789a5e9a004ac7f16d3913e6dced
tufel chauhan <tufel@promactinfo.com>
2019-02-06 17:45:32 +0530

    Assignment 1 added

---
+++ Assignment 1/Assignment1/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Employee
+    {
+        public Employee(int id,string name,string departmentname)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.DepartmentName = departmentname;
+        }
+
+        public int GetId()
+        {
+            MethodCalled("GetId");
+            return this.Id;
+        }
+        public string GetName()
+        {
+            MethodCalled("GetName");
+            return this.Name;
+        }
+        public string GetDepartmentName()
+        {
+            MethodCalled("GetDepartmentName");
+            return this.DepartmentName;
+        }
+        public void UpdateEmployee(int tmpId)
+        {
+            this.Id = tmpId;
+        }
+        public void UpdateEmployee(int tmpId,string tmpName)
+        {
+            this.Id = tmpId;
+            this.Name = tmpName;
+        }
+        public void UpdateEmployee(int tmpId, string tmpName,string tmpDepartmentName)
+        {
+            this.Id = tmpId;
+            this.Name = tmpName;
+            this.DepartmentName = tmpDepartmentName;
+        }
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private string DepartmentName { get; set; }
+        public event MethodCalledDelegate MethodCalled;
+    }
+}
---
+++ Assignment 1/Assignment1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tmpId;
+            string tmpName, tmpDepartmentName;
+            Console.WriteLine("Enter Employee Details");
+            Console.WriteLine("Enter Id : ");
+            tmpId = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Name : ");
+            tmpName = Console.ReadLine();
+            Console.WriteLine("Enter Department Name : ");
+            tmpDepartmentName = Console.ReadLine();
+
+            Console.WriteLine();
+            Employee emp1 = new Employee(tmpId,tmpName,tmpDepartmentName);
+            emp1.MethodCalled += new MethodCalledDelegate(onMethodCalled);
+            Console.WriteLine("Employee Details");
+            Console.WriteLine("Id : " + emp1.GetId());
+            Console.WriteLine("Name : " + emp1.GetName());
+            Console.WriteLine("Department : " + emp1.GetDepartmentName());
+
+            Console.WriteLine();
+            emp1.UpdateEmployee(11, "Tufel Chauhan", "IT Department");
+            Console.WriteLine("Updated Employee Details");
+            Console.WriteLine("Id : " + emp1.GetId());
+            Console.WriteLine("Name : " + emp1.GetName());
+            Console.WriteLine("Department : " + emp1.GetDepartmentName());
+        }
+        static void onMethodCalled(string methodName)
+        {
+            Console.WriteLine(methodName + " Method Called");
+        }
+    }
+}
